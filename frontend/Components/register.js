32ff2f18f2bbe289a7d6628c21524a0d704f7fde
Isabelle Chun <isabelle.chun@gmail.com>
2018-07-13 13:44:58 -0700

    frontend login and register

---
+++ frontend/Components/register.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button,
+  RefreshControl,
+  AsyncStorage } from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+export default class RegisterScreen extends React.Component {
+  render() {
+    return (
+      <View >
+      </View>
+    );
+  }
+}
